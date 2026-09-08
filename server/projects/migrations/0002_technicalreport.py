85dcd562f6ca819552967a65af3c42259a3c7054
Filip Plass <filipplass@gmail.com>
2026-09-08 17:34:27 +0200

    AAA

--- server/projects/migrations/0002_technicalreport.py
+++ server/projects/migrations/0002_technicalreport.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.db import migrations, models
 import django.db.models.deletion
 
-
+#radek
 class Migration(migrations.Migration):
 
     dependencies = [
